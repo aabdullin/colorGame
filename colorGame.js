587c3a8009f06fb95c37e216bd66c8649aae97f0
Artur <rutrika@gmail.com>
2017-09-26 22:39:42 -0700

    pickColor function

--- colorGame.js
+++ colorGame.js
@@ -5,25 +5,42 @@ var colors = [
   "rgb(0, 255, 255)",
   "rgb(0, 0, 255)",
   "rgb(255, 0, 255)"
-];
+]
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
 for(var i = 0; i < squares.length; i++){
 	// add initial colors to squares
-	squares[i].style.backgroundColor = colors[i];
+	squares[i].style.background = colors[i];
 	//add click listeners to squares
 	squares[i].addEventListener("click", function() {
 		var clickedColor = this.style.background;
 		//compare color to pickedColor
 		if(clickedColor === pickedColor) {
-			alert("Correct!")
+			messageDisplay.textContent = "Correct!";
+			changeColors(clickedColor);
 		} else {
-			alert("Wrong!")
+			this.style.background = "#232323";
+			messageDisplay.textContent = "Try Again";
 		}
 	});
+}
+
+function changeColors(color) {
+	//loop through all squares
+	for (var i = 0; i < squares.length; i++) {
+		// change each color to match given color
+		squares[i].style.background = color;
+	}
+	//change each color to match given color
+}
+
+function pickColor() {
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
 }
